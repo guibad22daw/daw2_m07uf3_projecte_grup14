98d21b969fa89ba68b529b27f4a67ab35361bb29
guibad22daw <guibad22daw@proton.me>
2023-04-27 10:23:23 +0200

    Creat seeder per Reserves

---
+++ database/seeders/ReservaSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ReservaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $llistaReserves = [
+            [
+                'dni' => '12345678N',
+                'codiHab' => '1234ABC',
+                'data_entrada' => '2023-04-27',
+                'data_sortida' => '2023-04-29',
+                'pensio' => 'Només allotjament',
+                'preu_dia' => '80',
+                'asseguranca' => 'Franquícia fins 500 Euros',
+            ],
+        ];
+        DB::table('reserves')->insert($llistaReserves);                  
+    }
+}
