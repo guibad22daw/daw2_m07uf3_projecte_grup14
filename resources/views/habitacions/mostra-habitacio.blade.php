d84ed41fc8b173e98e709359ad0c1dfd70c8d60d
guibad22daw <guibad22daw@proton.me>
2023-05-05 12:33:19 +0200

    Vistes per mostrar dades en concret millorades

--- resources/views/habitacions/mostra-habitacio.blade.php
+++ resources/views/habitacions/mostra-habitacio.blade.php
@@ -1,67 +1,70 @@
 @extends('disseny')
 @section('content')
-<h1>Dades del treballador</h1>
-<div class="mt-5">
-  <table class="table table-striped table-bordered table-hover">
-	<thead class="thead-dark">
-		<tr class="table-primary">
-			<th scope="col">CAMP</td>
-			<th scope="col">VALOR</td>
-        </tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td>Codi Habitació</td>
-			<td>{{$dades_habitacio->codiHab}}</td>
-		</tr>
-		<tr>
-			<td>Capacitat</td>
-			<td>{{$dades_habitacio->capacitat}}</td>
-		</tr>
-		<tr>
-			<td>Mida</td>
-			<td>{{$dades_habitacio->mida}}</td>
-		</tr>
-		<tr>
-			<td>Pensió</td>
-			<td>{{$dades_habitacio->pensio}}</td>
-		</tr>
-		<tr>
-			<td>Vistes</td>
-			<td>{{$dades_habitacio->vistes}}</td>
-		</tr>
-		<tr>
-			<td>Llits</td>
-			<td>{{$dades_habitacio->llits}}</td>
-		</tr>
-		<tr>
-			<td>Nombre de llits</td>
-			<td>{{$dades_habitacio->n_llits}}</td>
-		</tr>
-		<tr>
-			<td>Terrassa</td>
-			<td>{{$dades_habitacio->terrassa == "1" ? 'Sí':'No'}}</td>
-		</tr>
-		<tr>
-			<td>Calefacció</td>
-			<td>{{$dades_habitacio->calefaccio == "1" ? 'Sí':'No'}}</td>
-		</tr>
-		<tr>
-			<td>Aire acondicionat</td>
-			<td>{{$dades_habitacio->aire_acondicionat == "1" ? 'Sí':'No'}}</td>
-		</tr>
-		<tr>
-			<td>Nens</td>
-			<td>{{$dades_habitacio->nens == "1" ? 'Sí':'No'}}</td>
-		</tr>
-		<tr>
-			<td>Animals</td>
-			<td>{{$dades_habitacio->animals == "1" ? 'Sí':'No'}}</td>
-		</tr>
-	</tbody>	
-  </table>
-  <div class="p-6 bg-white border-b border-gray-200">
-	<a href="{{ url('dashboard') }}">Torna al dashboard<a/>                     
-  </div>  
+<h2 class="mt-5 text-center">Dades de l'habitació</h2>
+<p class="mt-3 text-center">Visualitza les dades de l'habitació</p>
+<div class="container" style="max-width: 850px;">
+	<table class="table table-striped table-bordered table-hover mt-5 shadow-sm sm:rounded-lg mostra" style="width: 100%; background-color:white; border-radius:20px; border: none">
+		<thead class="thead-dark">
+			<tr>
+				<th scope="col" style="width: 20%">CAMP</td>
+				<th scope="col">VALOR</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Codi Habitació</td>
+				<td>{{$dades_habitacio->codiHab}}</td>
+			</tr>
+			<tr>
+				<td>Capacitat</td>
+				<td>{{$dades_habitacio->capacitat}}</td>
+			</tr>
+			<tr>
+				<td>Mida</td>
+				<td>{{$dades_habitacio->mida}}</td>
+			</tr>
+			<tr>
+				<td>Pensió</td>
+				<td>{{$dades_habitacio->pensio}}</td>
+			</tr>
+			<tr>
+				<td>Vistes</td>
+				<td>{{$dades_habitacio->vistes}}</td>
+			</tr>
+			<tr>
+				<td>Llits</td>
+				<td>{{$dades_habitacio->llits}}</td>
+			</tr>
+			<tr>
+				<td>Nombre de llits</td>
+				<td>{{$dades_habitacio->n_llits}}</td>
+			</tr>
+			<tr>
+				<td>Terrassa</td>
+				<td>{{$dades_habitacio->terrassa == "1" ? 'Sí':'No'}}</td>
+			</tr>
+			<tr>
+				<td>Calefacció</td>
+				<td>{{$dades_habitacio->calefaccio == "1" ? 'Sí':'No'}}</td>
+			</tr>
+			<tr>
+				<td>Aire acondicionat</td>
+				<td>{{$dades_habitacio->aire_acondicionat == "1" ? 'Sí':'No'}}</td>
+			</tr>
+			<tr>
+				<td>Nens</td>
+				<td>{{$dades_habitacio->nens == "1" ? 'Sí':'No'}}</td>
+			</tr>
+			<tr>
+				<td>Animals</td>
+				<td>{{$dades_habitacio->animals == "1" ? 'Sí':'No'}}</td>
+			</tr>
+		</tbody>
+	</table>
+	<div style="display: flex; align-items: center; justify-content: center; gap: 20px;" class="mt-5">
+		<a href="{{ url('habitacions/visualitza') }}" style="width: 200px; border-radius: 15px;" class="btn btn-primary btn">
+			Torna enrere
+		</a>
+	</div>
 </div>
 @endsection
--- resources/views/reserves/mostra-reserva.blade.php
+++ resources/views/reserves/mostra-reserva.blade.php
@@ -1,11 +1,12 @@
 @extends('disseny')
 @section('content')
-<h1>Dades del treballador</h1>
-<div class="mt-5">
-    <table class="table table-striped table-bordered table-hover">
+<h2 class="mt-5 text-center">Dades de la reserva</h2>
+<p class="mt-3 text-center">Visualitza les dades de la reserva</p>
+<div class="container" style="max-width: 850px;">
+    <table class="table table-striped table-bordered table-hover mt-5 shadow-sm sm:rounded-lg mostra" style="width: 100%; background-color:white; border-radius:20px; border: none">
         <thead class="thead-dark">
-            <tr class="table-primary">
-                <th scope="col">CAMP</td>
+            <tr>
+                <th scope="col" style="width: 20%">CAMP</td>
                 <th scope="col">VALOR</td>
             </tr>
         </thead>
@@ -38,11 +39,12 @@
                 <td>Assegurança</td>
                 <td>{{$dades_reserva->asseguranca}}</td>
             </tr>
-            <tr>
         </tbody>
     </table>
-    <div class="p-6 bg-white border-b border-gray-200">
-        <a href="{{ url('dashboard') }}">Torna al dashboard<a />
+    <div style="display: flex; align-items: center; justify-content: center; gap: 20px;" class="mt-5">
+        <a href="{{ url('reserves/visualitza') }}" style="width: 200px; border-radius: 15px;" class="btn btn-primary btn">
+            Torna enrere
+        </a>
     </div>
 </div>
-        @endsection
+@endsection
